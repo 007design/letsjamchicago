fb3aefab6b3a89e5ef391aa5d01396512d568624
Daniel Hinds-Bond <007@007design.com>
2024-01-29 15:29:26 -0600

    whoops

--- app/controllers/email_controller.rb
+++ app/controllers/email_controller.rb
@@ -4,8 +4,8 @@ class EmailController < ApplicationController
   def index
     variable = Mailjet::Send.create(messages: [{
       'From'=> {
-        'Email'=> params[:email],
-        'Name'=> params[:from]
+        'Email'=> '007design.com@gmail.com',
+        'Name'=> 'Daniel Hinds-Bond'
       },
       'To'=> [
         {
@@ -14,7 +14,7 @@ class EmailController < ApplicationController
         }
       ],
       'Subject'=> "Let's Jam Chicago Contact Form",
-      'TextPart'=> params[:message],
+      'TextPart'=> "#{params[:message]}\n\nUser email: #{params[:email]}\nUser name: #{params[:from]}",
       # 'HTMLPart'=> '<h3>Dear passenger 1, welcome to <a href=\'https://www.mailjet.com/\'>Mailjet</a>!</h3><br />May the delivery force be with you!'
     }]
     )
